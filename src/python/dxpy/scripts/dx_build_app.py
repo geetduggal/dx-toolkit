4735a2b1736c842f5070e1d7a094417f1a69155b
pnguyen <pnguyen@dnanexus.com>
2016-11-02 10:18:34 -0700

    Clean up a bit.

--- src/python/dxpy/scripts/dx_build_app.py
+++ src/python/dxpy/scripts/dx_build_app.py
@@ -824,7 +824,6 @@ def build_and_upload_locally(src_dir, mode, overwrite=False, archive=False, publ
             logger.debug("Created temporary project %s to build in" % (working_project,))
 
         using_temp_project = True
-    # elif mode == "app" and not dry_run:
     elif not dry_run:
         # If we are not using temporary project(s) to build the app, then we
         # should have a project context somewhere.
@@ -833,9 +832,7 @@ def build_and_upload_locally(src_dir, mode, overwrite=False, archive=False, publ
             region = dxpy.api.project_describe(project,
                                                input_params={"fields": {"region": True}})["region"]
         except Exception:
-            # Defer raising until later.
-            print("\nDeferring...")
-            pass
+            err_exit()
         else:
             projects_by_region = {region: project}
 
@@ -864,8 +861,7 @@ def build_and_upload_locally(src_dir, mode, overwrite=False, archive=False, publ
                 region = dxpy.api.project_describe(dest_project,
                                                    input_params={"fields": {"region": True}})["region"]
             except Exception:
-                print("\nDeferring...")
-                pass
+                err_exit()
             else:
                 projects_by_region = {region: dest_project}
             for result in dxpy.find_data_objects(classname="applet", name=dest_name, folder=dest_folder,
